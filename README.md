29d7deb8340d20cad3af5b9051b8dd178ba872e5
Manuel Hanisch <manuelhanisch@gmail.com>
2016-09-18 01:05:33 +0200

    buildPixelGrid Function

--- README.md
+++ README.md
@@ -12,13 +12,36 @@ Use with node.js
 ```js
 var HexGrid = require('hexgridr');
 
-var hg = new HexGrid(hexCount).build(); // hexCount = number of Hexes
+var hg = new HexGrid(hexCount, orientation, layout, origin);
+hg.buildPixelGrid(hexWidth, hexHeight, originPosX, originPosY);
+
+//you can also chain both functions:
+var hg = new HexGrid(hexCount, orientation, layout, origin).buildPixelGrid(hexWidth, hexHeight, originPosX, originPosY);
+
+//Example:
+var hg = new HexGrid(20, "pointy-top", "hexagon", new Hex(0,0,0)).buildPixelGrid(64, 64, 256, 256);
 
 ```
-
 Check out the [Demo](http://hexgridr.manuha.work) page.
 
 
-PS: Hexgridr is currently under heavy development.
-More features and a complete documentation coming in
-the next days.
+This library uses internally a cubic representation (x,y,z) for all Hexes but it will
+be possible to convert them into an axial representation (x,y) soon!  
+
+
+Parameters Constructor:
+
+hexCount {int}: **0 to N**  *--Number of Hexes*  
+orientation {string}: **'pointy-top'** or **'flat-top'**  *--the two types of hexagons*  
+layout {string}: **'hexagon'**  - more coming soon (rectangle next)  
+origin {Hex}: a Hex with cubic coordinates x,y,z. You can use **new Hex(x,y,z)** or  **HexGrid.createHex(x,y,z)**. The Position of all Hexes will be relative to this one.
+
+Parameters buildPixelGrid: 
+
+hexWidth {int} : width of the Hex, usually in Pixels.  
+hexHeight {int}: height of the Hex, usually in Pixels.  
+originPosX {int}: X position of the origin Hex, usually in Pixels.   
+originPosX {int}: Y position of the origin Hex, usually in Pixels.   
+(Remark: Center is used as pivot point - hexWidth/2,  hexHeight/2).
+
+#### Hexgridr is currently under heavy development. More features and a complete documentation coming in the next days.
--- example/example.js
+++ example/example.js
@@ -1,23 +1,25 @@
 $(function() {
 
 
-  $("#hg_sel_hexnum").change(function() { hg_updateCodePreview() });
-  $("#hg_sel_orientation").change(function() { hg_updateCodePreview() });
-  $("#hg_sel_layout").change(function() { hg_updateCodePreview() });
-  $("#hg_sel_direction").change(function() { hg_updateCodePreview() });
-  $("#hg_sel_startingpoint").change(function() { hg_updateCodePreview() });
+  // $("#hg_sel_hexnum").change(function() { hg_updateCodePreview() });
+  // $("#hg_sel_orientation").change(function() { hg_updateCodePreview() });
+  // $("#hg_sel_layout").change(function() { hg_updateCodePreview() });
+  // $("#hg_sel_direction").change(function() { hg_updateCodePreview() });
+  // $("#hg_sel_startingpoint").change(function() { hg_updateCodePreview() });
+  //
+
+  $("#hg_sel_orientation,#hg_sel_layout,#hg_sel_direction,#hg_sel_startingpoint").change(function() { hg_updateCodePreview() });
+  $("#hg_sel_hexnum,#hg_sel_hexW,#hg_sel_hexH,#hg_sel_hexX,#hg_sel_hexY").on('input', function() { hg_updateCodePreview() });
+
+
 
   $("#hg_sel_createBtn").click(function() { hg_createHexGrid() });
 
   var hg_imgInfo_pointytop = {
-    name: 'hex_pointy_56_64.png',
-    width: 56,
-    height: 64
+    name: 'hex_pointy_56_64.png'
   }
   var hg_imgInfo_flattop = {
-    name: 'hex_flat_64_56.png',
-    width: 64,
-    height: 56
+    name: 'hex_flat_64_56.png'
   }
   //preloading images
   $("#hg_preloadimg_pointytop").css('backgroundImage','url('+hg_imgInfo_pointytop.name+')');
@@ -27,46 +29,43 @@ $(function() {
   function hg_createHexGrid() {
 
     var input = getInput();
-    var hg = new HexGrid(input.hexnum,input.orientation);
-    hg.build(input.layout);
-    hg_drawGrid(hg,input.orientation);
+    var hg = new HexGrid(input.hexnum,input.orientation, input.layout, HexGrid.createHex(0,0,0));
+
+
+
+    if(input.orientation == 'pointy-top') var imgInfo = hg_imgInfo_pointytop;
+    else var imgInfo = hg_imgInfo_flattop;
+
+    hg.buildPixelGrid(input.hexW, input.hexH, input.ohexX, input.ohexY);
+
+    hg_updateCodeResult(hg);
+
+    hg_drawPixelGrid(hg.pixelGrid, imgInfo.name);
 
   }
 
 
-  function hg_drawGrid(hg, orientation) {
+  function hg_drawPixelGrid(pGrid, imgName) {
 
-    var startX = 223;
-    var startY = 224;
-
-    hg_updateCodeResult(hg);
     $('#hg_stage').empty();
-
 
     var i = -1;
     (function loop(){
-      if (++i >= hg.grid.hexes.length) return;
+      if (++i >= pGrid.grid.length) return;
       setTimeout(function(){
-        hg_drawHex(hg.grid.hexes[i],i,startX,startY,orientation);
+        hg_drawHex(pGrid, i, imgName);
         loop();
       }, 100);
     })();
 
   }
-
-
-  function hg_drawHex(hexy,someId,offsetX,offsetY,orientation) {
-
-    if(orientation == 'pointy-top') var imgInfo = hg_imgInfo_pointytop;
-    else var imgInfo = hg_imgInfo_flattop;
-
-    var pp = hexy.getPixelPosition(imgInfo.width, imgInfo.height, offsetX, offsetY, orientation);
+  function hg_drawHex(pGrid, index, imgName) {
 
     $("<div>")
     .attr({
       'class': "animated fadeIn",
-      'id': "hg_stagehex_"+someId,
-      'style': "position:absolute;left:"+pp.x+"px;top:"+pp.y+"px;width:"+imgInfo.width+"px;height:"+imgInfo.height+"px;background-image: url("+imgInfo.name+");background-repeat: no-repeat;background-position: right top;"
+      'id': "hg_stagehex_"+index,
+      'style': "position:absolute;left:"+pGrid.grid[index].x+"px;top:"+pGrid.grid[index].y+"px;width:"+pGrid.hexWidth+"px;height:"+pGrid.hexHeight+"px;background-size:100% 100%;background-image: url("+imgName+");background-repeat: no-repeat;background-position: right top;"
     })
     .appendTo('#hg_stage');
 
@@ -74,14 +73,23 @@ $(function() {
 
 
   function hg_updateCodePreview() {
+
     var input = getInput();
-    var code = 'var hg = new HexGrid('+input.hexnum+',"'+input.orientation+'").build();';
+    var code = 'var hg = new HexGrid('+input.hexnum+', "'+input.orientation+'", "'+input.layout+'", HexGrid.createHex(0,0,0));<br />hg.buildPixelGrid('+input.hexW+', '+input.hexH+', '+input.ohexX+', '+input.ohexY+');';
+    code += '<br /><br />/*you can also chain both functions:*/<br /><br />';
+    code += 'var hg = new HexGrid('+input.hexnum+', "'+input.orientation+'", "'+input.layout+'", HexGrid.createHex(0,0,0)).buildPixelGrid('+input.hexW+', '+input.hexH+', '+input.ohexX+', '+input.ohexY+');';
+
     $("#hg_code_build").html(code);
+    hljs.highlightBlock($('#hg_code_build')[0]);
+
+
+
   }
 
 
   function hg_updateCodeResult(hg) {
-    $("#hg_code_result").html('HexGrid: '+JSON.stringify(hg));
+    $("#hg_code_result").html('HexGrid: '+JSON.stringify(hg)+'<br />PixelGrid: '+JSON.stringify(hg.pixelGrid));
+    hljs.highlightBlock($('#hg_code_result')[0]);
   }
 
 
@@ -90,6 +98,10 @@ $(function() {
     input.hexnum = parseInt($('#hg_sel_hexnum').val());
     input.orientation = $('#hg_sel_orientation').val().toLowerCase();
     input.layout =  $('#hg_sel_layout').val().toLowerCase();
+    input.hexW = parseInt($('#hg_sel_hexW').val());
+    input.hexH = parseInt($('#hg_sel_hexH').val());
+    input.ohexX = parseInt($('#hg_sel_hexX').val());
+    input.ohexY = parseInt($('#hg_sel_hexY').val());
     return input;
   }
 
